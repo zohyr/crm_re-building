13bd2367e4658740fe79e983e443ca0b98299e67
zoyhr <z0hyrrash@gmail.com>
2020-07-24 12:02:53 +0200

    simple html worked finegit status

--- marketing_sec/urls.py
+++ marketing_sec/urls.py
@@ -1,2 +1,11 @@
 from django.urls import path
+from . import views
 
+
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('customer/', views.customer, name="c"),
+    path('products/', views.product, name="p"),
+    path('status/', views.status, name="s")
+
+]
--- marketing_sec/views.py
+++ marketing_sec/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'dashboard.html', {})
+
+def customer(request):
+    return render(request, 'customer.html', {})
+
+
+def product(request):
+    return render(request, 'products.html', {})
+
+def status(request):
+    return render(request, 'status.html', {})
+    
